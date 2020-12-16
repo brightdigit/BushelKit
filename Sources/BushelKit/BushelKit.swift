11a39a9f2378d06b3f4df244ab609a41df3d3925
Leo Dion <leogdion@brightdigit.com>
2020-12-15 21:17:16 -0500

    initial commit

---
+++ Sources/BushelKit/BushelKit.swift
@@ -0,0 +1,3 @@
+struct BushelKit {
+  var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+import BushelKitTests
+
+var tests = [XCTestCaseEntry]()
+tests += BushelKitTests.__allTests()
+
+XCTMain(tests)
