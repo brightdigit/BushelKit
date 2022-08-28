76664dd408ba569719d62a24e8ff136e4e19407d
Leo Dion <leogdion@brightdigit.com>
2022-08-28 22:05:41 +0000

    cleaning up unused code and adding single window for welcome and remote images

--- Packages/BushelKit/Sources/BushelUI/MockImageContainer.swift
+++ Packages/BushelKit/Sources/BushelUI/MockImageContainer.swift
@@ -1,7 +1,7 @@
 //
 // MockImageContainer.swift
 // Copyright (c) 2022 BrightDigit.
-// Created by Leo Dion on 8/12/22.
+// Created by Leo Dion on 8/21/22.
 //
 
 import BushelMachine
--- Packages/BushelKit/Sources/BushelUI/UI/BushelApplication.swift
+++ Packages/BushelKit/Sources/BushelUI/UI/BushelApplication.swift
@@ -1,13 +1,15 @@
 //
 // BushelApplication.swift
 // Copyright (c) 2022 BrightDigit.
-// Created by Leo Dion on 8/12/22.
+// Created by Leo Dion on 8/21/22.
 //
 
 import SwiftUI
 
+// periphery:ignore
 public protocol BushelApplication: App {}
 
+// periphery:ignore
 public extension BushelApplication {
   var body: some Scene {
     BushelScene()
