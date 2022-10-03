7943cd59af8b6d14a5979a6f8b19fe4e6f594a87
Leo Dion <leogdion@brightdigit.com>
2022-10-03 15:20:38 -0400

    working on fixing save dialog for machine

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-if [ -z "$SRCROOT" ]; then
-	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-	PACKAGE_DIR="${SCRIPT_DIR}/../Packages/BushelKit"
-else
-	PACKAGE_DIR="${SRCROOT}/Packages/BushelKit" 	
-fi
-
-if [ "$STRICT_MODE" == "true" ] || [ "$STRICT_MODE" = "YES" ]; then
+if [ "$LINT_MODE" == "NONE" ]; then
+	exit
+elif [ "$LINT_MODE" == "STRICT" ]; then
 	SWIFTFORMAT_OPTIONS=""
 	SWIFTLINT_OPTIONS="--strict"
 	STRINGSLINT_OPTIONS="--config .strict.stringslint.yml"
@@ -15,6 +10,13 @@ else
 	SWIFTFORMAT_OPTIONS=""
 	SWIFTLINT_OPTIONS=""
 	STRINGSLINT_OPTIONS="--config .stringslint.yml"
+fi
+
+if [ -z "$SRCROOT" ]; then
+	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+	PACKAGE_DIR="${SCRIPT_DIR}/../Packages/BushelKit"
+else
+	PACKAGE_DIR="${SRCROOT}/Packages/BushelKit" 	
 fi
 
 pushd $PACKAGE_DIR
