e0fc5804523996852f4e2cf01677438852d415d1
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-10-05 02:02:59 +0000

    Removing and Migrating Old Wax Data

--- Packages/BushelKit/Sources/BushelCoreWax/Fake/String.swift
+++ Packages/BushelKit/Sources/BushelCoreWax/Fake/String.swift
@@ -12,4 +12,13 @@ public extension String {
   static let restoreImageIdentiferSample: Self = "C90C2C17-7CA1-466C-93C0-D73591D65C94:2480CC13-8CFE-4CB6-9FBF-FFD2157B8995"
   static let imageIDSample: Self = "2480CC13-8CFE-4CB6-9FBF-FFD2157B8995"
   static let libraryBookmarkIDSample: Self = UUID.bookmarkIDSample.uuidString
+
+  static let lowerCaseAlphaNumberic = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+  static func randomLowerCaseAlphaNumberic(ofLength length: Int = 32) -> String {
+    String((1 ... length).map { _ in
+      // swiftlint:disable:next force_unwrapping
+      lowerCaseAlphaNumberic.randomElement()!
+    })
+  }
 }
