f3f4370ad65402c347533ec0814da03156a6dbb3
Leo Dion <leogdion@brightdigit.com>
2023-11-15 18:14:55 +0000

    Cleaning up downloading

---
+++ Packages/BushelKit/Sources/BushelLibraryViews/Observation/DocumentObject+Selection.swift
@@ -0,0 +1,160 @@
+//
+// DocumentObject+Selection.swift
+// Copyright (c) 2023 BrightDigit.
+//
+
+#if canImport(SwiftUI)
+  import BushelCore
+  import BushelLibrary
+  import Foundation
+  import SwiftData
+  extension DocumentObject {
+    func onHubImageSelected() {
+      guard let selectedHubImage else {
+        return
+      }
+      let url = selectedHubImage.url
+      guard let object else {
+        Self.logger.error("Error missing library file.")
+        let error = LibraryError.missingInitializedProperty(.bookmarkData)
+        assertionFailure(error: error)
+        self.error = error
+        return
+      }
+      object.beginImport(
+        .remote(url: url, metadata: selectedHubImage.metadata)
+      ) {
+        self.restoreImageImportProgress = $0
+      } onError: { error in
+        Self.logger.error("Unable to import \(url): \(error)")
+        self.error = assertionFailure(error: error) { error in
+          Self.logger.critical("Unknown error: \(error)")
+        }
+      }
+    }
+
+    func restoreImageFileFrom(result: Result<URL, Error>) -> LibraryFile? {
+      let newResult: Result<URL, LibraryError>
+      do {
+        newResult = try assertionFailure(result: result)
+      } catch {
+        Self.logger.critical("Unknown Error: \(error)")
+        return nil
+      }
+      switch newResult {
+      case let .failure(error):
+        Self.logger.error("Error importing restore image: \(error)")
+
+        assertionFailure(error: error)
+        self.error = error
+        return nil
+
+      case let .success(url):
+        Self.logger.debug("Importing Restore Image Selected: \(url)")
+        return .init(url: url)
+      }
+    }
+
+    func onFileImporterCompleted(_ result: Result<URL, Error>) {
+      let newResult: Result<URL, LibraryError>
+      do {
+        newResult = try assertionFailure(result: result)
+      } catch {
+        Self.logger.critical("Unknown Error: \(error)")
+        return
+      }
+      switch (newResult, object) {
+      case let (.failure(error), _):
+        Self.logger.error("Error importing restore image: \(error)")
+        assertionFailure(error: error)
+        self.error = error
+
+      case (_, .none):
+        Self.logger.error("Error missing library file.")
+        let error = LibraryError.missingInitializedProperty(.bookmarkData)
+        assertionFailure(error: error)
+        self.error = error
+
+      case let (.success(url), .some(object)):
+        Self.logger.debug("Importing Restore Image Selected: \(url)")
+        object.beginImport(
+          .local(url: url)
+        ) {
+          self.restoreImageImportProgress = $0
+        } onError: { error in
+          Self.logger.error("Unable to import \(url): \(error)")
+          self.error = assertionFailure(error: error) { error in
+            Self.logger.critical("Unknown error: \(error)")
+          }
+        }
+      }
+    }
+
+    func queueRemovalSelectedItem() {
+      guard let selectedItem else {
+        Self.logger.error("There is no selected item to queue.")
+        assertionFailure("There is no selected item to queue.")
+        return
+      }
+
+      queuedRemovalSelectedImageID = selectedItem
+      self.presentDeleteImageConfirmation = true
+    }
+
+    func cancelRemovalSelectedItem(withID id: UUID) {
+      guard let selectedItem else {
+        Self.logger.error("There is no selected item to remove from queue.")
+        assertionFailure("There is no selected item to remove from queue.")
+        return
+      }
+
+      assert(selectedItem == id)
+      self.queuedRemovalSelectedImageID = nil
+    }
+
+    func deleteSelectedItem(withID id: UUID) {
+      guard let selectedItem else {
+        Self.logger.error("There is no selected item to delete.")
+        assertionFailure("There is no selected item to delete.")
+        return
+      }
+
+      guard let object else {
+        Self.logger.error("There is object to delete from.")
+        assertionFailure("There is object to delete from.")
+        return
+      }
+
+      assert(selectedItem == id)
+      do {
+        Self.logger.debug("Deleting \(selectedItem)")
+        try object.deleteImage(withID: selectedItem)
+      } catch {
+        Self.logger.error("Unable to delete image: \(error)")
+        self.error = assertionFailure(error: error) { error in
+          Self.logger.critical("Unknown error: \(error)")
+        }
+      }
+      self.queuedRemovalSelectedImageID = nil
+      self.selectedItem = nil
+    }
+
+    @MainActor
+    func onSelectionChange() {
+      guard let object else {
+        Self.logger.error("There is object to select from.")
+        assertionFailure("There is object to select from.")
+        return
+      }
+
+      do {
+        try object.save()
+      } catch {
+        Self.logger.error("Unable to save library: \(error)")
+        self.error = assertionFailure(error: error) { error in
+          Self.logger.critical("Unknown error: \(error)")
+        }
+      }
+    }
+  }
+#endif
---
+++ Packages/BushelKit/Sources/BushelLibraryViews/Observation/LibraryObject+Extensions.swift
@@ -0,0 +1,94 @@
+//
+// LibraryObject+Extensions.swift
+// Copyright (c) 2023 BrightDigit.
+//
+
+#if canImport(SwiftUI)
+  import BushelCore
+  import BushelLibrary
+  import BushelLibraryData
+  import BushelProgressUI
+  import Foundation
+  import SwiftData
+  import SwiftUI
+  extension LibraryObject {
+    @MainActor
+    // swiftlint:disable:next cyclomatic_complexity
+    func save() throws {
+      guard let modelContext else {
+        throw LibraryError.missingInitializedProperty(.modelContext)
+      }
+
+      guard let bookmarkData = entry.bookmarkData else {
+        throw LibraryError.missingInitializedProperty(.bookmarkData)
+      }
+
+      defer {
+        do {
+          try bookmarkData.update(using: modelContext)
+        } catch {
+          assertionFailure(error: error)
+        }
+      }
+
+      let libraryURL: URL
+      do {
+        libraryURL = try bookmarkData.fetchURL(using: modelContext, withURL: nil)
+      } catch {
+        throw try LibraryError.bookmarkError(error)
+      }
+
+      let canAccessFile = libraryURL.startAccessingSecurityScopedResource()
+
+      guard canAccessFile else {
+        throw LibraryError.accessDeniedError(nil, at: libraryURL)
+      }
+
+      let jsonFileURL = libraryURL.appending(path: URL.bushel.paths.restoreLibraryJSONFileName)
+      do {
+        let jsonData = try JSON.encoder.encode(library)
+        try jsonData.write(to: jsonFileURL)
+      } catch {
+        throw LibraryError.metadataUpdateError(error, at: jsonFileURL)
+      }
+
+      do {
+        try modelContext.save()
+      } catch {
+        throw LibraryError.fromDatabaseError(error)
+      }
+    }
+
+    func bindableImage(withID id: UUID?) -> Bindable<LibraryImageObject>? {
+      guard let id else {
+        Self.logger.debug("No id for image")
+        return nil
+      }
+      guard let index = library.items.firstIndex(where: { $0.id == id }) else {
+        Self.logger.error("Unable to find child image with id: \(id)")
+        assertionFailure("Unable to find child image with id: \(id)")
+        return nil
+      }
+      let entryChild = self.entry.images?.first(where: { $0.imageID == id })
+      let entry: LibraryImageEntry?
+      do {
+        entry = try entryChild ?? modelContext?.fetch(
+          FetchDescriptor<LibraryImageEntry>(
+            predicate: #Predicate { $0.imageID == id }
+          )
+        ).first
+      } catch {
+        Self.logger.error("Error fetching entry \(id) from database: \(error)")
+        assertionFailure(error: error)
+        return nil
+      }
+      guard let entry else {
+        Self.logger.error("No entry with \(id) from database")
+        assertionFailure("No entry with \(id) from database")
+        return nil
+      }
+
+      return .init(LibraryImageObject(index: index, library: self, entry: entry))
+    }
+  }
+#endif
