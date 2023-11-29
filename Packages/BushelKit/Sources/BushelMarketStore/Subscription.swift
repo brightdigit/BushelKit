a4ce55d8161268778856daacacdd64d990218a67
Leo Dion <leogdion@brightdigit.com>
2023-11-29 01:44:34 +0000

    Adding Support for Subscriptions Offline

--- Packages/BushelKit/Sources/BushelMarketStore/Subscription.swift
+++ Packages/BushelKit/Sources/BushelMarketStore/Subscription.swift
@@ -11,21 +11,38 @@
   import StoreKit
 
   public extension Subscription {
-    init(groupID: String, renewal: Product.SubscriptionInfo.RenewalInfo, products: [Product.ID: Product]) {
+    init?(transaction: Transaction) async {
+      let renewalInfo = try? await transaction.subscriptionStatus?.renewalInfo.payloadValue
+      let groupID = transaction.subscriptionGroupID
+
+      guard let groupID, transaction.revocationDate == nil else {
+        return nil
+      }
+
+      self.init(
+        groupID: groupID,
+        currentProductID: transaction.productID,
+        transactionDate: transaction.signedDate,
+        environment: .init(transaction.environment),
+        willAutoRenew: renewalInfo?.willAutoRenew,
+        recentSubscriptionStartDate: renewalInfo?.recentSubscriptionStartDate ?? transaction.purchaseDate,
+        renewalDate: renewalInfo?.renewalDate ?? transaction.expirationDate
+      )
+    }
+
+    init(
+      groupID: String,
+      renewal: Product.SubscriptionInfo.RenewalInfo,
+      products: [Product.ID: Product],
+      transactionDate: Date = .init()
+    ) {
       let product = products[renewal.currentProductID]
       let subscriptionPeriod = product?.subscription?.subscriptionPeriod
-
-      let currentProductID = renewal.currentProductID
-      let productID = String(describing: product?.id)
-
-      assert(
-        subscriptionPeriod != nil,
-        "Missing subscription period: \(currentProductID) \(productID)"
-      )
 
       self.init(
         groupID: groupID,
         currentProductID: renewal.currentProductID,
+        transactionDate: transactionDate,
         environment: .init(renewal.environment),
         willAutoRenew: renewal.willAutoRenew,
         recentSubscriptionStartDate: renewal.recentSubscriptionStartDate,
