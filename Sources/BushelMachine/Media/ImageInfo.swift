db9eeaad3755e76ab4dc5e7d93e7d3d944e5c2ec
Leo Dion <leogdion@brightdigit.com>
2024-10-07 20:59:53 -0400

    working on UI for Recordings

--- Sources/BushelMachine/Media/ImageInfo.swift
+++ Sources/BushelMachine/Media/ImageInfo.swift
@@ -9,9 +9,16 @@ public import Foundation
 
 
 extension ImageInfo {
-  init(imageUUID: UUID, size: CGSize, fileSize: UInt64, configuration: CaptureImage.Configuration) {
+  init(imageUUID: UUID,
+       name : String,
+notes : String,
+createdAt : Date,
+       size: CGSize, fileSize: UInt64, configuration: CaptureImage.Configuration) {
     self.init(
       imageUUID: imageUUID,
+      name: name,
+      notes: notes,
+      createdAt: createdAt,
       width: .init(size.width),
       height: .init(size.height),
       fileSize: fileSize,
@@ -30,8 +37,20 @@ public struct ImageInfo : Sendable, Codable{
     case missingField(Field)
     case innerError(Error)
   }
-  internal init(imageUUID: UUID, width: Int, height: Int, fileSize: UInt64, configuration: CaptureImage.Configuration) {
+  internal init(
+    imageUUID: UUID,
+    name: String,
+    notes: String,
+    createdAt: Date,
+    width: Int,
+    height: Int,
+    fileSize: UInt64,
+    configuration: CaptureImage.Configuration
+  ) {
     self.imageUUID = imageUUID
+    self.name = name
+    self.notes = notes
+    self.createdAt = createdAt
     self.width = width
     self.height = height
     self.fileSize = fileSize
@@ -39,6 +58,9 @@ public struct ImageInfo : Sendable, Codable{
   }
   
   public let imageUUID: UUID
+  public let name : String
+  public let notes : String
+  public let createdAt : Date
   public let width : Int
   public let height : Int
   public let fileSize : UInt64
@@ -91,6 +113,9 @@ public struct ImageFileParser : ImageInfoParser {
     
     return .init(
       imageUUID: imageUUID,
+      name: "",
+      notes: "",
+      createdAt: Date(),
       size: size,
       fileSize: fileSize,
       configuration: image.configuration
--- Sources/BushelMachine/Media/VideoInfo.swift
+++ Sources/BushelMachine/Media/VideoInfo.swift
@@ -70,33 +70,60 @@ public protocol VideoParser  {
 public struct VideoInfo : Sendable, Codable {
   public init(
     videoUUID: UUID,
+    name: String,
+    createdAt : Date,
+    notes : String,
     fileSize: UInt64,
     width: Int,
     height: Int,
     duration: Double,
+    imageTimeInterval : Double,
     configuration: CaptureVideoConfiguration
   ) {
     self.videoUUID = videoUUID
+    self.name = name
+    self.createdAt = createdAt
+    self.notes = notes
     self.fileSize = fileSize
     self.width = width
     self.height = height
     self.duration = duration
+    self.imageTimeInterval = imageTimeInterval
     self.configuration = configuration
   }
   
   public init(
     videoUUID: UUID,
+    name: String,
+    createdAt : Date,
+    notes : String,
     fileSize: UInt64,
     size: CGSize,
     duration: Double,
+    imageTimeInterval : Double,
     configuration: CaptureVideoConfiguration
   ) {
-    self.init(videoUUID: videoUUID, fileSize: fileSize, width: Int(size.width), height: Int(size.height), duration: duration, configuration: configuration)
+    self.init(
+      videoUUID: videoUUID,
+      name: name,
+      createdAt: createdAt,
+      notes: notes,
+      fileSize: fileSize,
+      width: Int(size.width),
+      height: Int(size.height),
+      duration: duration,
+      imageTimeInterval: imageTimeInterval,
+      configuration: configuration
+    )
   }
   public let videoUUID: UUID
+  public let name : String
+  public let createdAt : Date
+  public let notes : String
   public let fileSize: UInt64
   public let width: Int
   public let height: Int
+  public let imageTimeInterval : Double
   public let duration: Double
   public let configuration : CaptureVideoConfiguration
 }
