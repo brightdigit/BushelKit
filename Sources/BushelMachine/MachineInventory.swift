dae3137040ced24db4ea03827575fd83a0e8a615
Leo Dion <leogdion@brightdigit.com>
2025-03-31 13:19:46 -0400

    working on inventory

--- Sources/BushelMachine/MachineInventory.swift
+++ Sources/BushelMachine/MachineInventory.swift
@@ -8,6 +8,45 @@
 public import Observation
 import Foundation
 
+extension MachineProperties {
+  init (original: MachineProperties, propertyChange: any PropertyChange) {
+    let type = type(of: propertyChange)
+    
+    var state : MachineState = original.state
+    var canStart : Bool = original.canStart
+    var canStop : Bool = original.canStop
+    var canPause : Bool = original.canPause
+    var canResume = original.canResume
+    var canRequestStop = original.canRequestStop
+    
+    switch type.property {
+    case .state:
+      state = propertyChange.getValue() ?? state
+    case .canStart:
+      canStart = propertyChange.getValue() ?? canStart
+    case .canStop:
+      canStop = propertyChange.getValue() ?? canStop
+    case .canPause:
+      canPause = propertyChange.getValue() ?? canPause
+    case .canResume:
+      canResume = propertyChange.getValue() ?? canResume
+    case .canRequestStop:
+      canRequestStop = propertyChange.getValue() ?? canRequestStop
+    }
+    self.init(
+      state: state,
+      canStart: canStart,
+      canStop: canStop,
+      canPause: canPause,
+      canResume: canResume,
+      canRequestStop: canRequestStop
+    )
+  }
+  func updateProperty(_ property: any PropertyChange) -> MachineProperties {
+    .init(original: self, propertyChange: property)
+  }
+}
+
 @Observable
 public final class MachineInventory : Sendable  {
   
@@ -21,7 +60,19 @@ public final class MachineInventory : Sendable  {
     let machine : any Machine
     let observationID : UUID
     var properties : MachineProperties
+    
+    mutating func updatedProperties(from changes: MachineChange) {
+      if let properties = changes.properties {
+        self.properties = properties
+      }
+      
+      if case let .property(property) = changes.event {
+        self.properties = self.properties.updateProperty(property)
+      }
+    }
   }
+  
+
   
   public init() {
     
@@ -40,8 +91,15 @@ public final class MachineInventory : Sendable  {
   }
   
   nonisolated func machineWithID(_ id: UUID, updatedTo changes: MachineChange) {
-    
+    Task { @MainActor in
+      self.onMachine(withID: id, updatedTo: changes)
+    }
   }
   
+  @MainActor
+  func onMachine(withID id: UUID, updatedTo changes: MachineChange) {
+    assert(self.registeredMachines[id] != nil)
+    self.registeredMachines[id]?.updatedProperties(from: changes)
+  }
   
 }
