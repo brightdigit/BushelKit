383886abcf60d0397723d0b254b44e9519ea4df8
Leo Dion <leogdion@brightdigit.com>
2025-05-30 15:48:44 -0400

    fixup! fixing machine strings

--- Sources/BushelVirtualBuddy/VirtualBuddyError.swift
+++ Sources/BushelVirtualBuddy/VirtualBuddyError.swift
@@ -27,3 +27,11 @@
 //  OTHER DEALINGS IN THE SOFTWARE.
 //
 
+public import Foundation
+
+public enum VirtualBuddyError: Error {
+  case unsupportedURL(URL)
+  case networkError(URLError)
+  case decodingError(DecodingError)
+  case unknownError(Error)
+}
